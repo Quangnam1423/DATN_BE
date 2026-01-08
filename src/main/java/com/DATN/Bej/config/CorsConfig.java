bab636a8ff3ce250c1922aef558b7d3b247e3c7a
itwasmehyu <huyxiem123@gmail.com>
2026-01-08 22:42:53 +0700

    sua thanh toan

--- src/main/java/com/DATN/Bej/config/CorsConfig.java
+++ src/main/java/com/DATN/Bej/config/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
+        // config.setAllowedOriginPatterns(List.of("*")); // Thay đổi từ setAllowedOrigins
         config.setAllowedOrigins(List.of("http://localhost:5173")); // Cho phép frontend truy cập
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
